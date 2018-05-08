76ed6285fc273c8cc58307e041fdccbd13bd1685
josua.frank <josua.frank@RWLWIM009.computerkomplett.de>
2018-05-08 13:03:18 +0200

    first checker methods

---
+++ src/main/java/teamcoffee/softwarequalityproject/checkers/Salutation.java
@@ -0,0 +1,13 @@
+package teamcoffee.softwarequalityproject.checkers;
+
+/**
+ *
+ * @author Josua Frank
+ */
+public class Salutation {
+    
+    public static boolean isSalutation(String string){
+        return false;
+    }
+    
+}
---
+++ src/main/java/teamcoffee/softwarequalityproject/checkers/Title.java
@@ -0,0 +1,13 @@
+package teamcoffee.softwarequalityproject.checkers;
+
+/**
+ *
+ * @author Josua Frank
+ */
+public class Title {
+    
+    public static boolean isTitle(String string){
+        return false;
+    }
+    
+}
